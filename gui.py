868f1fbd219c4a0edb43aa082152dc2833628f2a
Clemens <61827340+cpfluger@users.noreply.github.com>
2022-05-16 18:38:56 +0200

    drag & drop konzept

--- gui.py
+++ gui.py
@@ -1,1 +1,28 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
 
+
+class MainWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Drag and Drop")
+        self.resize(720, 480)
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        files = [u.toLocalFile() for u in event.mimeData().urls()]
+        for f in files:
+            print(f)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = MainWidget()
+    ui.show()
+    sys.exit(app.exec_())
